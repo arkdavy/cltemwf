00a28ca2f852cecd453c9cb196d276a0db4ce296
arkdavy <arkadiy.davydov@warwick.ac.uk>
2022-06-28 15:03:47 +0100

    example fix

--- examples/run_batch.sh
+++ examples/run_batch.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
+# prerequisites: 
+# (1) clTEM_cmd installed on the system
+# (2) cltemwf code installed (e.g., with 'pip install .' from the project's root)
+# (3) (cif)text files imported form Windows has to be converted with "tr -d '\r' < Si_win.cif > Si_lin.cif" command to fix the Windows-specific line endings
+
+# Add clTEM_cmd into the environent if installed with the module system
 module purge
 module load GCC/10.2.0 CUDA/11.1.1 OpenMPI/4.0.5
 module load clTEM/0.3.4
 
-export PATH=../:${PATH}
-export PYTHONPATH=../:{$PYTHONPATH}
-
-# the whole config can be provided as a string of dictionary, or via a path to a new config.
+# the whole config can be provided as a string of dictionary, as here, or via a path to a new config.
 config='{"ctem": {"cropped padding": true}, "default padding": {"xy": {"units": "\u00c5", "val": 8.0}, "z": {"units": "\u00c5", "val": 3.0}}, "double precision": false, "filename": "None.cif", "full 3d": {"state": false}, "intermediate output": {"enabled": false, "slice interval": 0}, "maintain areas": false, "microscope": {"aberrations": {"C10": {"units": "nm", "val": 0.0}, "C12": {"ang": 0.0, "mag": 0.0, "units": "nm, \u00b0"}, "C21": {"ang": 0.0, "mag": 0.0, "units": "nm, \u00b0"}, "C23": {"ang": 0.0, "mag": 0.0, "units": "nm, \u00b0"}, "C30": {"units": "\u03bcm", "val": 1.0}, "C32": {"ang": 0.0, "mag": 0.0, "units": "\u03bcm, \u00b0"}, "C34": {"ang": 0.0, "mag": 0.0, "units": "\u03bcm, \u00b0"}, "C41": {"ang": 0.0, "mag": 0.0, "units": "\u03bcm, \u00b0"}, "C43": {"ang": 0.0, "mag": 0.0, "units": "\u03bcm, \u00b0"}, "C45": {"ang": 0.0, "mag": 0.0, "units": "\u03bcm, \u00b0"}, "C50": {"units": "\u03bcm", "val": 0.0}, "C52": {"ang": 0.0, "mag": 0.0, "units": "\u03bcm, \u00b0"}, "C54": {"ang": 0.0, "mag": 0.0, "units": "\u03bcm, \u00b0"}, "C56": {"ang": 0.0, "mag": 0.0, "units": "\u03bcm, \u00b0"}}, "aperture": {"units": "mrad", "val": 20.0}, "aperture smooth radius": {"units": "mrad", "val": 0.0}, "beam tilt": {"azimuth": {"units": "\u00b0", "val": 0.0}, "inclination": {"units": "mrad", "val": 0.0}}, "voltage": {"units": "kV", "val": 200.0}}, "mode": {"id": 2, "name": "STEM"}, "potentials": "kirkland", "resolution": 1024, "simulation area": {"x": {"finish": 10.0, "padding": 5.0, "start": 0.0, "units": "\u00c5"}, "y": {"finish": 10.0, "padding": 5.0, "start": 1.0, "units": "\u00c5"}, "z": {"finish": 10.0, "padding": 5.0, "start": 1.0, "units": "\u00c5"}}, "slice count": 10, "slice offset": {"units": "\u00c5", "val": 0.96}, "slice thickness": {"val": 1.92}, "stem": {"area": {"padding": {"units": "\u00c5", "val": 0.0}, "x": {"finish": 18.2799, "start": 10.6, "units": "\u00c5"}, "y": {"finish": 15.5305, "start": 10.1, "units": "\u00c5"}}, "concurrent pixels": 1, "detectors": {"ADF": {"centre": {"units": "mrad", "x": 0.0, "y": 0.0}, "radius": {"inner": 50.0, "outer": 180.0, "units": "mrad"}}}, "scan": {"x": {"pixels": 77}, "y": {"pixels": 54}}}}'
 
-# its elements can be updated via extra config, which must not change the default config structure, 
-# i.e., it should keep the depth and the number of fields unchanged
+# its elements can be updated via extra config string, which must not change the default config structure, 
+# i.e., it should keep the depth, the number and names of fields unchanged
 config_extra='{"slice thickness":{"val": 1.92}}'
 
 # remove spaces between entries, and place '_' instead of the key's spaces. This is required for arguments parsed safely
@@ -27,11 +31,14 @@ if $bashiface; then
  
   # the example below shows the usage of the code within the batch script
   echo "Running bash interface"
+  
+  # the helper from clTEM itself
+  clTEM_cmd --help
 
-  # there is a helper function, which describes flags recognised by cltemwf
+  # cltemwf_batch has also a helper function, which describes flags recognised by cltemwf
   cltemwf_batch --help
 
-  # call to cltemwf. The extra configuration flag takes a dictionary here, which will be merge onto the default configuration.
+  # call to cltemwf. The extra configuration flag takes a dictionary here, which will be merged onto the default configuration.
   cltemwf_batch --cif Si.cif --config_extra ${config_extra} --files-to-copy '*.tif' --outdir 'outdir' -d gpu -s 2,2,2 -z 0,0,1
 
   # default config is provided by a file on the $config_path
@@ -48,6 +55,3 @@ else
   python python_iface.py
 
 fi
-
-
-
